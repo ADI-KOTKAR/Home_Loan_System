332300653ae83319ce901d9511c17e6ecec4b1bc
karthi_7408 <50882125+KARTHIKEYANJM@users.noreply.github.com>
2022-08-24 09:25:40 +0530

    v3 commit

--- Home Loan System.sql
+++ Home Loan System.sql
@@ -1,22 +1,6 @@
 create database home_loan;
 use home_loan;
 
--- Note:Foreign Key is not used here as it may cause some problems during a transaction,
--- so for simplicity purposes I'm dropping the option of using foreign key
-
-
--- Note: For loading an image following command should be used
--- INSERT INTO loan_application (doc) VALUES(LOAD_FILE('/image_path/image_fileName.png'));
-
--- signup table
-create table signup(id int auto_increment, username varchar(100) not null, 
-phone_no varchar(10) not null unique, mail varchar(50) not null unique, 
-pass varchar(50) not null, confirm_pass varchar(50) not null, state varchar(50) not null,
-city varchar(50) not null, country varchar(50) not null,pincode varchar(10),primary key(id));
-
-create database home_loan;
-use home_loan;
-
 -- drop database home_loan;
 
 -- drop table signup;
@@ -111,18 +95,13 @@ constraint fk_sav_acc_3 foreign key(saving_acc_no) references signup(saving_acc_
 constraint fk_loan_acc_1 foreign key(loan_id) references loan_application(loan_id)
 );
 
-create table credit(id int auto_increment, saving_acc_no int not null unique,amount double not null,balance double not null,
+create table transaction(id int auto_increment, saving_acc_no int not null unique,amount double not null,balance double not null,
 primary key(id),
 constraint fk_sav_acc_4 foreign key(saving_acc_no) references signup(saving_acc_no),
 constraint fk_bal foreign key(balance) references savings_acc(balance)
 );
 
-create table debit(id int auto_increment, saving_acc_no int not null unique,amount double not null,balance double not null,
-primary key(id),
-constraint fk_sav_acc_5 foreign key(saving_acc_no) references signup(saving_acc_no),
-constraint fk_bal_1 foreign key(balance) references savings_acc(balance)
-);
+
 -- create table transaction(trans_id int auto_increment,loan_id int not null unique, 
 -- savings_acc_no int not null unique, balance double not null, primary key(trans_id));
-
  
